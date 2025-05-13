b9786512f997613ed580f3bf17beb2041340d67b
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-13 17:18:32 -0300

    Class with components. Able to change properties with event onChange, this, target.value and setState

---
+++ exercises/src/components/greetings.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+
+export default class Greeting extends Component{
+
+
+    //create to able to change the value type
+    state = {
+        type: this.props.type,
+        name: this.props.name
+    }
+
+    setType(e){
+        // console.log(e.target.value) //target = input
+        // this.props.type = e.target.value; //change the value of type
+        this.setState({type: e.target.value}) //able to change the value, and the component is updated
+    }
+
+    setName(e){
+        this.setState({name: e.target.value}) //able to change the value, and the component is updated
+    }
+
+    render(){
+        const {type, name} = this.state;
+        return(
+            <div>
+                <h1>{type} {name}!</h1>
+                <hr />
+                <input type="text" placeholder="Type..." 
+                value={type} onChange={e => this.setType(e)}></input>
+                <input type="text" placeholder="Name..." 
+                value={name} onChange={e => this.setName(e)}></input>
+            </div>
+        )
+    }
+}
--- exercises/src/index.js
+++ exercises/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'; //render the first element of the page
 
         //First letter must be uppercase, because I created
 // import First from './components/first';
+
+//****COMPONENT WITH PARAMETERS****///
 // import GoodMorning from './components/goodMorning';
 // const content = <GoodMorning name="Gui" age={25}/>; //react is used with .jsx
 
-import Multi, {GoodAfternoon} from './components/multiples'
-const content = <div><GoodAfternoon name="Ana"/> <Multi.GoodEvening name="Gui"/></div>
+
+//****IMPORT MULTIPLE COMPONENTS****///
+// import Multi, {GoodAfternoon} from './components/multiples'
+// const content = <div><GoodAfternoon name="Ana"/> <Multi.GoodEvening name="Gui"/></div>
+
+//****COMPONENTS AS CLASS****///
+import Greeting from "./components/greetings";
+const content = <Greeting type="Good Morning" name="Jonas"/>; //these parameters are only readbly
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //talks with the page
 root.render(content);
