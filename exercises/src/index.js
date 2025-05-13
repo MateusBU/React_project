36784b0e9aa3184c4004c7efe866ef11285d386b
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-13 15:16:36 -0300

    Using ReactDOM to access the page

--- exercises/src/index.js
+++ exercises/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom/client'; //render the first element of the page
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import First from './components/first'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const content = <h1>React</h1>; //react is used with .jsx
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //talks with the page
+root.render(content);
+ //lauches <h1>..., inside the element of id root
