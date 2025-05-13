5066439b77a1962f3835ed205c30d6bdcbea4b7f
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-13 16:37:35 -0300

    Feature to export multiple components

---
+++ exercises/src/components/multiples.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const GoodAfternoon = props => <h1>Good Afternoon, {props.name}!!</h1>
+const GoodEvening = props => <h1>Good Evening, {props.name}!!</h1>
+export {GoodEvening, GoodAfternoon}
+export default{GoodEvening, GoodAfternoon}
--- exercises/src/index.js
+++ exercises/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from 'react-dom/client'; //render the first element of the page
 
         //First letter must be uppercase, because I created
-import First from './components/first';
-import GoodMorning from './components/goodMorning';
+// import First from './components/first';
+// import GoodMorning from './components/goodMorning';
+// const content = <GoodMorning name="Gui" age={25}/>; //react is used with .jsx
 
-const content = <GoodMorning name="Gui" age={25}/>; //react is used with .jsx
+import Multi, {GoodAfternoon} from './components/multiples'
+const content = <div><GoodAfternoon name="Ana"/> <Multi.GoodEvening name="Gui"/></div>
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //talks with the page
 root.render(content);
